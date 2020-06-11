a35bf5ff855a0f0b9115c81f1f750202fe884d14
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-06-11 23:03:06 +0900

    make function insertionSortRecursive

--- test002.c
+++ test002.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+void printIntArray(const int *A, int n) {
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", A[i]);
+    putchar('\n');
+    return;
+}
+
 void printBin(int x) {
     int A[64];
     int i, j;
@@ -9,19 +17,46 @@ void printBin(int x) {
         A[i] = x & 1;
         x >>= 1;
     }
-    for (j = i - 1; j >= 0; j--) 
+    for (j = i - 1; j >= 0; j--)
         printf("%d", A[j]);
     putchar('\n');
+    return;
+}
+
+int *makeArrayRand(int n) {
+    int *A;
+    int i;
+    A = (int *)malloc(n * sizeof(int));
+    for (i = 0; i < n; i++) {
+        A[i] = rand() % 100;
+    }
+    return A;
+}
+
+void insertionSortRecursive(int *A, int n) {
+    if (n > 1) {
+        insertionSortRecursive(A, n - 1);
+        int key = A[n - 1];
+        int i;
+        for (i = n - 2; i >= 0 && A[i] > key; i--)
+            A[i + 1] = A[i];
+        A[i + 1] = key;
+    }
     return;
 }
 
 int main(void) {
     srand((unsigned)time(NULL));
     int r;
+    int l = 16;
+    int *sample1 = makeArrayRand(l);
     r = rand();
     printf("%ld\n", sizeof r);
     printf("%d\n", r);
     printf("%x\n", r);
     printBin(r);
+    printIntArray(sample1, l);
+    insertionSortRecursive(sample1, l);
+    printIntArray(sample1, l);
     return 0;
 }
