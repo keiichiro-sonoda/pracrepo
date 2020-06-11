56856859fc808f1b341792cbbca67d56354148be
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-06-12 00:49:44 +0900

    test.c
    arrayRandom added

--- test.c
+++ test.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define arrayLength(A) sizeof(A) / sizeof(A[0])
 
@@ -22,11 +24,28 @@ void insertionSort(int *A, int n) {
     return;
 }
 
+int *arrayRandom(int n);
+
 int main(void) {
-    int sample1[] = {5, 7, 6, 8, 2, 1, 9, 3, 4, 0};
+    srand((unsigned)time(NULL));
+    int sample1[] = {5, 7, 6, 8, 2, 1, 9, 3, 4, 0, 11, 6, 7, 12, 1, 14, 9, 10};
     int l = arrayLength(sample1);
+    int *sample2 = arrayRandom(l);
     printIntArray(sample1, l);
     insertionSort(sample1, l);
     printIntArray(sample1, l);
+    printIntArray(sample2, l);
+    insertionSort(sample2, l);
+    printIntArray(sample2, l);
     return 0;
+}
+
+int *arrayRandom(int n) {
+    int *A;
+    A = (int *)malloc(n * sizeof(int));
+    if (A == NULL) return NULL;
+    int i;
+    for (i = 0; i < n; i++)
+        A[i] = rand() % 100;
+    return A;
 }
