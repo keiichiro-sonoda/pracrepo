0de78305952067ecad4bd59e8c50c1ab5489cc2c
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-06-12 23:38:13 +0900

    make initBoard

--- othello.c
+++ othello.c
@@ -840,55 +840,52 @@ Board normalBoard(Board b1) {
     Board b2, b3, b4, b5, b6, b7, b8, bm;
     b2 = rotL90DegBoard(b1);
     bm = minBoard(b1, b2);
-    showBoard(b2);
-    showBoard(bm);
+    //showBoard(b2);
+    //showBoard(bm);
     b3 = rotL90DegBoard(b2);
     bm = minBoard(bm, b3);
-    showBoard(b3);
-    showBoard(bm);
+    //showBoard(b3);
+    //showBoard(bm);
     b4 = rotL90DegBoard(b3);
     bm = minBoard(bm, b4);
-    showBoard(b4);
-    showBoard(bm);
+    //showBoard(b4);
+    //showBoard(bm);
     b5 = mirrorHLBoard(b1);
     bm = minBoard(bm, b5);
-    showBoard(b5);
-    showBoard(bm);
+    //showBoard(b5);
+    //showBoard(bm);
     b6 = mirrorHLBoard(b2);
     bm = minBoard(bm, b6);
-    showBoard(b6);
-    showBoard(bm);
+    //showBoard(b6);
+    //showBoard(bm);
     b7 = mirrorHLBoard(b3);
     bm = minBoard(bm, b7);
-    showBoard(b7);
-    showBoard(bm);
+    //showBoard(b7);
+    //showBoard(bm);
     b8 = mirrorHLBoard(b4);
     bm = minBoard(bm, b8);
-    showBoard(b8);
-    showBoard(bm);
+    //showBoard(b8);
+    //showBoard(bm);
     return bm;
+}
+
+// initial configure
+void initBoard(void) {
+    START.board[1] = 0x0000000000000180L;
+    START.board[0] = 0x0240000000000000L;
+    return;
 }
 
 // main
 int main(void) {
-    // initial configure
-    START.board[0] = 0x0240000000000000;
-    START.board[1] = 0x0000000000000180;
+    initBoard();
     int i, j;
-    Board nbs[NEXT_MAX];
     // sample boards
     Board sample1, sample2;
     sample1.board[0] = 0xaaaa2aa902aa5541;
     sample1.board[1] = 0x00000000000000aa;
     sample2.board[0] = 0xaaaa28a90aaa5545;
     sample2.board[1] = 0x0000200209021202;
-    showBoard(sample2);
-    //sample2 = normalBoard(sample2);
-    showBoard(sample2);
-    sample2 = swapBoard(sample2);
-    showBoard(sample2);
-    kugiri()
-    sample2 = normalBoard(sample2);
-    //showBoard(sample2);
+    showBoard(START);
     return 0;
 }
