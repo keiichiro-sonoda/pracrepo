334835e1736b55c8deaaa38ed788e56281ef38ab
keiichiro-sonoda <mountain13577749@gmail.com>
2020-06-27 23:33:29 +0900

    等差数列を作る関数 range() を作成

--- win003.c
+++ win003.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+// 等差数列で初期化する関数
+void range(int *A, int n, int start, int step) {
+    for (int i = 0; i < n; i++) {
+        A[i] = start + i * step;
+    }
+}
+
 int main(void) {
     int size = 10;
     int i, a[size], b[size], c[size];
 
-    for (i = 0; i < size; i++) {
-        a[i] = i + 1;
-        b[i] = 1 - i;
-    }
-    
+    range(a, size, 1, 1);
+    range(b, size, 1, -1);
+
     for (i = 0; i < size; i++) {
         c[i] = a[i] + b[i];
     }
