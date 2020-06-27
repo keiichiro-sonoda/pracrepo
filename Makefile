49a5ff4ae44da9bc21433d34ea00b5afd7f73177
keiichiro-sonoda <mountain13577749@gmail.com>
2020-06-27 23:58:43 +0900

    Makefile 使うとき .exe 付けないとあかんらしい

--- Makefile
+++ Makefile
@@ -16,3 +16,5 @@ sort01_win.o: sort01_win.c
 	gcc sort01_win.c -c
 win002.so: win002.c
 	gcc win002.c -o win002.so -shared -fPIC
+win003.exe: win003.c array.c
+	gcc win003.c array.c -o win003
--- win003.c
+++ win003.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
+#include "array.h" // <> ではなく "" で囲う
 
-// プロトタイプ宣言
-void printDecimalArray(const int *A, int n);
-void range(int *A, int n, int start, int step);
-void init(int *A, int n, int num);
-void zeros(int *A, int n);
-
+// array.h と array.c のおかげでシンプルになった!
+// コンパイル時に array.c も組み合わせなければならないので
+// Makefile を活用するべき?
 int main(void) {
     int size; size = 5;
 
@@ -28,36 +26,4 @@ int main(void) {
     printDecimalArray(C, size);
 
     return 0;
-}
-
-// int型の配列を表示
-// これはデバッグでよく使われるらしい
-// 誰もが通る道なのか..
-void printDecimalArray(const int *A, int n) {
-    putchar('{');
-    for (int i = 0; i < n; i++) {
-        printf("%3d", A[i]);
-        if (i + 1 < n) {
-            printf(", ");
-        }
-    }
-    printf("}\n");
-}
-
-// 等差数列で初期化する関数
-void range(int *A, int n, int start, int step) {
-    for (int i = 0; i < n; i++) {
-        A[i] = start + i * step;
-    }
-}
-
-// 全ての要素を特定の数字で初期化
-// 初項n, 交差0の等差数列で考える(気付かなかった)
-void init(int *A, int n, int num) {
-    range(A, n, num, 0);
-}
-
-// 全零配列特化(せっかくなのでinitを使う)
-void zeros(int *A, int n) {
-    init(A, n, 0);
 }
