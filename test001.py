08412e15fd345710e556b8f23ea30135f75a6be7
keiichiro-sonoda <mountain13577749@gmail.com>
2020-06-27 17:08:42 +0900

    コンピュータの名前が表示された! 17T2088B-PC 見覚えがある奴

--- test001.py
+++ test001.py
@@ -5,10 +5,25 @@ import sys
 import ctypes
 
 user32 = ctypes.WinDLL("user32")
+kernel32 = ctypes.WinDLL("kernel32")
+
 # A/Wの区別がある場合はWを使う??
 user32.MessageBoxW.restype = ctypes.c_int32
 user32.MessageBoxW.argtypes = (
     ctypes.c_void_p, ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32
 )
 
-user32.MessageBoxW(None, u"テスト", u"タイトル", 0)
+#user32.MessageBoxW(None, u"テスト", u"タイトル", 0)
+
+# 関数を取り出しちゃおう
+GetComputerNameW = kernel32.GetComputerNameW
+GetComputerNameW.restype = ctypes.c_bool
+GetComputerNameW.argtypes = (
+    ctypes.c_wchar_p, ctypes.POINTER(ctypes.c_uint32)
+)
+
+lenComputerName = ctypes.c_uint32()
+GetComputerNameW(None, lenComputerName)
+computerName = ctypes.create_unicode_buffer(lenComputerName.value)
+GetComputerNameW(computerName, lenComputerName)
+print(computerName.value)
