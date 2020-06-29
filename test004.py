ae9b030755699adf1efa03fb48a09f1de44f5bcf
keiichiro-sonoda <mountain13577749@gmail.com>
2020-06-29 17:32:16 +0900

    複数のグラフを同時に表示してみた

--- test004.py
+++ test004.py
@@ -20,15 +20,40 @@ def getSprmFileWrap(n):
     getSprmFile(n, f_arr_c)
     return list(f_arr_c)
 
+def multGraphSample():
+    # x 全部同じやん
+    x_1 = np.linspace(0, 1, 100)
+    y_1 = x_1
+    x_2 = np.linspace(0, 1, 100)
+    y_2 = x_2 ** 2
+    x_3 = np.linspace(0, 1, 100)
+    y_3 = x_3 ** 3
+    x_4 = np.linspace(0, 1, 100)
+    y_4 = x_4 ** 4
+    # ウィンドウ的な何か
+    fig = plt.figure()
+
+    # 2 x 2 の枠にグラフを描画?
+    # 左上
+    ax_1 = fig.add_subplot(221)
+    # 右上
+    ax_2 = fig.add_subplot(222)
+    # 左下
+    ax_3 = fig.add_subplot(223)
+    # 右下
+    ax_4 = fig.add_subplot(224)
+
+    ax_1.plot(x_1, y_1)
+    ax_2.plot(x_2, y_2)
+    ax_3.plot(x_3, y_3)
+    ax_4.plot(x_4, y_4)
+    plt.show()
+
+    print("end?")
+
 if __name__ == "__main__":
     l1 = getSprmFileWrap(300)
     #print(l1)
     x = np.linspace(0, 1, 100)
     y = x ** 2
-    # オブジェクト指向で
-    # ウィンドウ的な
-    fig = plt.figure()
-    # 一つのグラフ的な
-    ax = fig.add_subplot(111)
-    ax.plot(x, y)
-    plt.show()
+    multGraphSample()
