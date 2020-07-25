3efd5d78472adcfb91bc00a72bfcb7dd95b28292
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-07-26 00:50:26 +0900

    nextGenerationSprmNM can be used

--- genetic03.c
+++ genetic03.c
@@ -47,13 +47,82 @@ void copyFirstGeneration(void) {
     fclose(fp);
 }
 
+// make next generation file
+// for no mutant file
+int nextGenerationSprmNM(int gene_num) {
+    int i, j, count;
+    // file name for not mutate
+    char format[] = "prm/sprm_not_mutate%03d.bin";
+    char fnamer[FILENAME_MAX], fnamew[FILENAME_MAX];
+    // previous survivors
+    Sprm parents[SURVIVE_NUM];
+    FILE *fp;
+    // print file name for reading
+    snprintf(fnamer, FILENAME_MAX, format, gene_num);
+    // print file name for writing
+    snprintf(fnamew, FILENAME_MAX, format, gene_num + 1);
+    // view
+    printf("read file : %s\n", fnamer);
+    printf("write file: %s\n", fnamew);
+    // read parents
+    if ((fp = fopen(fnamer, "rb")) == NULL) {
+        printf("\a%s can't be opened\n", fnamer);
+        return -1;
+    }
+    // opened!
+    fread(parents, sizeof parents, 1, fp);
+    fclose(fp);
+    // check write file (can read?)
+    if ((fp = fopen(fnamew, "rb")) != NULL) {
+        printf("\a%s exists. Can I overwrite it? (y): ", fnamew);
+        fclose(fp);
+        // don't overwrite
+        if (getchar() != 'y') {
+            printf("terminated\n");
+            return -1;
+        }
+    }
+    // make children!
+    Sprm generation[GENE_NUM];
+    // 10 parents copy
+    for (count = 0; count < SURVIVE_NUM; count++) {
+        generation[count] = parents[count];
+    }
+    // make children
+    // 45 combinations, 2 children per couple
+    for (i = 0; i < SURVIVE_NUM - 1; i++) {
+        for (j = i + 1; j < SURVIVE_NUM; j++) {
+            // average
+            generation[count] = makeChildAverageSprm(parents[i], parents[j]);
+            count++;
+            // cross (not mutate)
+            generation[count] = makeChildCrossNMSprm(parents[i], parents[j]);
+            count++;
+        }
+    }
+    // calcurate survivors
+    Sprm survivors[SURVIVE_NUM];
+    // battle!
+    getSurvivorSprm(generation, survivors);
+
+    // write current survivors
+    if ((fp = fopen(fnamew, "wb")) == NULL) {
+        printf("\a%s cannot be opened\n", fnamew);
+        return -1;
+    }
+    // opened!
+    fwrite(survivors, sizeof survivors, 1, fp);
+    fclose(fp);
+
+    return 0;
+}
+
 int main(void) {
     // initial configurations
     initBoard();
     setIndexes();
     srand((unsigned)time(NULL));
 
-    copyFirstGeneration();
-    
+    nextGenerationSprmNM(0);
     return 0;
 }
