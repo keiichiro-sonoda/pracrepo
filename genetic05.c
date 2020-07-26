853ee921523a9fe643cc90bb03e07b26161893ea
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-07-26 17:46:59 +0900

    sprmVSRandomNormal() was created

--- genetic05.c
+++ genetic05.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "genetic02.h"
 
+// play against random
+// return winner
+int sprmVSRandomNormal(const Sprm *prp, int my_turn) {
+    Board nba[NEXT_MAX];
+    int kc[3];
+    int pass = 0;
+    int n, dif;
+    // set turn
+    int turn = 0b01;
+    // set initial board
+    Board main_board = START;
+    while (1) {
+        // calculate next (and normalize)
+        n = nextBoardNormal2(main_board, nba, kc);
+        // can't put a piece anywhere
+        if (n == 0) {
+            // can't do anything one another
+            if (pass) {
+                //printf("end\n");
+                break;
+            }
+            // pass
+            swapNormalizeBoard(&main_board);
+            turn ^= 0b11;
+            pass = 1;
+            continue;
+        }
+        pass = 0;
+        // determine a next board
+        // parameter's turn
+        if (turn == my_turn) {
+            main_board = getBestBoardForBlackSimple(nba, n, prp);
+        } // random turn
+        else {
+            // randomly choose a next board
+            main_board = nba[rand() % n];
+        }
+        // switch turn
+        turn ^= 0b11;
+    }
+    // difference between black and white
+    dif = kc[1] - kc[2];
+    if (dif > 0) return turn;
+    if (dif < 0) return turn ^ 0b11;
+    // draw
+    return 0;
+}
+
 int main(void) {
-    printString(B2C);
+    // initial configurations
+    initBoard();
+    setIndexes();
+    srand((unsigned)time(NULL));
+
+    printString("test");
     return 0;
 }
