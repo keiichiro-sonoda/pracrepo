b52735917805afbea337e63e262d87481c77aa1c
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-07-26 15:21:46 +0900

    showBoard() successed

--- genetic04.c
+++ genetic04.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include "genetic02.h"
 
+// define the corner evaluation
+#define CORNER_EVAL 0.5
+
 // copy the first generation
 // and set the corner evaluation 0.5
 void copyFGFixCorner1(void) {
@@ -21,7 +24,7 @@ void copyFGFixCorner1(void) {
     for (int i = 0; i < SURVIVE_NUM; i++) {
         printFloatArray(pa[i].weight, SPRM_LEN);
         // fix the corner
-        pa[i].weight[0] = 0.5;
+        pa[i].weight[0] = CORNER_EVAL;
     }
     printString("fix!!");
     // reconfirmation
@@ -42,8 +45,112 @@ void copyFGFixCorner1(void) {
     fclose(fp);
 }
 
+// calculate average (fix the corner)
+Sprm makeChildAverageSprmFC1(Sprm mother, Sprm father) {
+    Sprm child;
+    // the corner
+    child.weight[0] = CORNER_EVAL;
+    // the others
+    for (int i = 1; i < SPRM_LEN; i++) {
+        child.weight[i] = (mother.weight[i] + father.weight[i]) / 2;
+    }
+    return child;
+}
+
+// cross parameters (fix the corner)
+// the mutation rate is 5%
+Sprm makeChildCrossMSprmFC1(Sprm mother, Sprm father) {
+    Sprm child;
+    // the corner
+    child.weight[0] = CORNER_EVAL;
+    // the others
+    for (int i = 1; i < SPRM_LEN; i++) {
+        // rate 5%
+        child.weight[i] = fcrossMFlex(mother.weight[i], father.weight[i], 0.05);
+    }
+    return child;
+}
+
+// make next generation file
+// fix the evaluation value of corner to 0.5
+int nextGenerationSprmFC1(int gene_num) {
+    int i, j, count;
+    // file name for fix the corner
+    char format[] = "prm/sprm_not_mutate%03d.bin";
+    char fnamer[FILENAME_MAX], fnamew[FILENAME_MAX];
+    // previous survivors
+    Sprm parents[SURVIVE_NUM];
+    FILE *fp;
+    // print file name for reading
+    snprintf(fnamer, FILENAME_MAX, format, gene_num);
+    // print file name for writing
+    snprintf(fnamew, FILENAME_MAX, format, gene_num + 1);
+    // view
+    printf("read file : %s\n", fnamer);
+    printf("write file: %s\n", fnamew);
+    // read parents
+    if ((fp = fopen(fnamer, "rb")) == NULL) {
+        printf("\a%s can't be opened\n", fnamer);
+        return -1;
+    }
+    // opened!
+    fread(parents, sizeof parents, 1, fp);
+    fclose(fp);
+    // check write file (can read?)
+    if ((fp = fopen(fnamew, "rb")) != NULL) {
+        printf("\a%s exists. Can I overwrite it? (y): ", fnamew);
+        fclose(fp);
+        // don't overwrite
+        if (getchar() != 'y') {
+            printString("terminated.");
+            return -1;
+        }
+    }
+    // make children!
+    Sprm generation[GENE_NUM];
+    // 10 parents copy
+    for (count = 0; count < SURVIVE_NUM; count++) {
+        generation[count] = parents[count];
+    }
+    // make children (fix the corner)
+    // 45 combinations, 2 children per couple
+    for (i = 0; i < SURVIVE_NUM - 1; i++) {
+        for (j = i + 1; j < SURVIVE_NUM; j++) {
+            // average
+            generation[count] = makeChildAverageSprmFC1(parents[i], parents[j]);
+            count++;
+            // cross (not mutate)
+            generation[count] = makeChildCrossMSprmFC1(parents[i], parents[j]);
+            count++;
+        }
+    }
+    // calcurate survivors
+    Sprm survivors[SURVIVE_NUM];
+    // battle!
+    getSurvivorSprm(generation, survivors);
+    // print parameters
+    printString("top ten parameters:");
+    for (i = 0; i < SURVIVE_NUM; i++) {
+        printf("%2d: ", i + 1);
+        printFloatArray(survivors[i].weight, SPRM_LEN);
+    }
+
+    // write current survivors
+    if ((fp = fopen(fnamew, "wb")) == NULL) {
+        printf("\a%s cannot be opened\n", fnamew);
+        return -1;
+    }
+    // opened!
+    fwrite(survivors, sizeof survivors, 1, fp);
+    fclose(fp);
+
+    return 0;
+}
+
 int main(void) {
     setIndexes();
-    copyFGFixCorner1();
+    initBoard();
+    showBoard(START);
+    showBoardHex(START);
     return 0;
 }
