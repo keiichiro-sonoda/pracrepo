a7541c816434144226d9b084d9b25e948cfdb4a7
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-07-26 15:06:32 +0900

    prm/sprm_corner0.5_000.bin was created

--- genetic04.c
+++ genetic04.c
@@ -2,8 +2,48 @@
 #include <stdio.h>
 #include "genetic02.h"
 
+// copy the first generation
+// and set the corner evaluation 0.5
+void copyFGFixCorner1(void) {
+    FILE *fp;
+    // file name for reading
+    char fnamer[] = "prm/simple_prm000.bin";
+    if ((fp = fopen(fnamer, "rb")) == NULL) {
+        // failed
+        printf("%s can't be opened.\n", fnamer);
+        return;
+    }
+    // opened!
+    Sprm pa[SURVIVE_NUM];
+    fread(pa, sizeof pa, 1, fp);
+    fclose(fp);
+    // check parameters
+    for (int i = 0; i < SURVIVE_NUM; i++) {
+        printFloatArray(pa[i].weight, SPRM_LEN);
+        // fix the corner
+        pa[i].weight[0] = 0.5;
+    }
+    printString("fix!!");
+    // reconfirmation
+    for (int i = 0; i < SURVIVE_NUM; i++) {
+        printFloatArray(pa[i].weight, SPRM_LEN);
+    }
+    // file name for writing
+    char fnamew[] = "prm/sprm_corner0.5_000.bin";
+    // open a file to write (or make a file)
+    if ((fp = fopen(fnamew, "wb")) == NULL) {
+        // failed
+        printf("%s can't be opened.\n", fnamew);
+        return;
+    }
+    // opened!
+    fwrite(pa, sizeof pa, 1, fp);
+    // close
+    fclose(fp);
+}
+
 int main(void) {
     setIndexes();
-    printDecimalArray(INDEXES, MASU_NUM);
+    copyFGFixCorner1();
     return 0;
 }
