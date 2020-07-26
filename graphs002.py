1350642689d4630132c0da7c948154d27fe6fd87
keiichiro-sonoda <mountain13577749@gmail.com>
2020-07-27 02:39:53 +0900

    ランダム対戦で適応度を評価した場合のグラフを作成できた pythonでファイル名指定してファイル読み込みに成功した

--- graphs002.py
+++ graphs002.py
@@ -32,6 +32,10 @@ getTop10FC1Ave = exe2_win.getTop10FC1AvePy
 getTop10FC1Ave.rectype = None
 getTop10FC1Ave.argtypes = (c_int32, FloatArray10)
 
+getTop10AveFlex = exe2_win.getTop10AveFlexPy
+getTop10AveFlex.rectype = None
+getTop10AveFlex.argtypes = (c_char_p, FloatArray10)
+
 def getSprmFileWrap(n):
     f_arr_c = FloatArray64()
     getSprmFile(n, f_arr_c)
@@ -50,6 +54,13 @@ def getTop10NMAveWrap(n):
 def getTop10FC1AveWrap(n):
     f_arr_c = FloatArray10()
     getTop10FC1Ave(n, f_arr_c)
+    return list(f_arr_c)
+
+# 文字列を渡す
+def getTop10AveFlexWrap(fnamer):
+    f_arr_c = FloatArray10()
+    # バイト型に直して与える
+    getTop10AveFlex(fnamer.encode(), f_arr_c)
     return list(f_arr_c)
 
 # グラフ用の色
@@ -222,6 +233,54 @@ def dataView03():
     ax.set_ylabel("point", fontsize=15)
     plt.show()
 
+# 平均値表示したい(ランダム対戦適応計算)
+def dataView04():
+    fname_format = "prm/sprm_vsrand{:03d}.bin"
+    x_min = 0
+    x_max = 300
+    x = []
+    # 10 マス分のデータの配列を用意
+    ys = [[] for i in range(10)]
+    for i in range(x_min, x_max + 1):
+        # x は範囲内の整数全て
+        x.append(i)
+        # i 世代のトップ10の平均値を取り出す
+        tprm = getTop10AveFlexWrap(fname_format.format(i))
+        # それぞれのマスの評価値に代入!
+        for j in range(10):
+            ys[j].append(tprm[j])
+    
+    # 使い慣れたいからオブジェクト指向にしよう
+    fig = plt.figure(figsize=(8, 5))
+    ax = fig.add_subplot(
+        111,
+        xlabel="generation",
+        ylabel="point",
+    )
+    # 各マスの変移をプロット
+    for i in range(10):
+        lw = 1
+        lc = LINE_COLORS[i]
+        if i in [0, 4]:
+            lw = 4
+        # ラベル付け
+        ax.plot(x, ys[i],
+            label="{:d}".format(i + 1),
+            color=lc,
+            linewidth=lw
+        )
+    #plt.legend(loc="best")
+    # 凡例調節
+    ax.legend(
+        bbox_to_anchor=(1.01, 1),
+        loc='upper left',
+        borderaxespad=0,
+        fontsize=10
+    )
+    ax.set_xlabel("generation", fontsize=15)
+    ax.set_ylabel("point", fontsize=15)
+    plt.show()
+
 if __name__ == "__main__":
     #dataView02()
-    dataView03()
+    dataView04()
