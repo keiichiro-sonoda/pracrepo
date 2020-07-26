b5ce9d8014ba7c99a58facd28f9b2d3e4a5bccec
keiichiro-sonoda <mountain13577749@gmail.com>
2020-07-26 16:36:22 +0900

    test005.py を追加した

---
+++ test005.py
@@ -0,0 +1,18 @@
+# 突然変異除いたバージョンのパラメータのグラフを作りたい
+from ctypes import *
+
+# 共有ライブラリ読み込み(同じディレクトリで実行)
+exe2_win = cdll.LoadLibrary("exe2_win.so")
+# 配列型定義
+FloatArray64 = c_float * 64
+FloatArray10 = c_float * 10
+
+# 初期化関数を実行
+exe2_win.initPy()
+# 関数取り出し
+printString = exe2_win.printStringPy
+printString.rectype = None
+printString.argtypes = (c_char_p)
+
+if __name__ == "__main__":
+    printString(b"abc")
