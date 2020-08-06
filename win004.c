c568e557ff523f49f215a3c4141872f4e6f53f18
keiichiro-sonoda <mountain13577749@gmail.com>
2020-08-06 19:33:14 +0900

    srand() を使っても乱数が変わらない。 複数ファイル使う場合は扱い方が違うのか?

--- win004.c
+++ win004.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "array.h"
 
 int main(void) {
     int n = 10;
     int A[n];
     randArray(A, n);
+    srand((unsigned)time(NULL));
     printDecimalArray(A, n);
     return 0;
 }
