e46b38b0add3fa1ab894ce0a0fd8b3099f5af1b8
keiichiro-sonoda <mountain13577749@gmail.com>
2020-08-06 19:59:54 +0900

    ルーレット選択っぽい関数ができたかも? 何度も繰り返して確率を確かめよう

--- win004.c
+++ win004.c
@@ -4,8 +4,25 @@
 #include "array.h"
 
 // ルーレット選択
+// 配列の要素は非負の整数であること前提
 int roulette(int *A, int n) {
-    return 0;
+    // 合計値を計算
+    int s = sum(A, n);
+    // 0 以上合計値未満の乱数を生成
+    int r = rand() % s;
+    int i = 0;
+    while (i < n) {
+        // 要素を引いていく
+        // 大きい値ほど選ばれやすい?
+        r -= A[i];
+        // 0になったら終了
+        if (r <= 0) {
+            break;
+        }
+        i++;
+    }
+    // 選ばれた添え字を返す
+    return i;
 }
 
 int main(void) {
@@ -14,6 +31,6 @@ int main(void) {
     srand((unsigned)time(NULL));
     range(A, n, 0, 1);
     printDecimalArray(A, n);
-    printDecimal(sum(A, n));
+    printDecimal(roulette(A, n));
     return 0;
 }
