def1b6b3595a2314d1392658cc8d7853ee592ba7
keiichiro-sonoda <mountain13577749@gmail.com>
2020-08-22 21:35:27 +0900

    整数型配列の和を求める関数を作成 誰でも作れるような関数を何度も作って楽しいか? 謎の自問自答 後悔しているリポジトリだろ pushしてないからまだ見ないかもだけど

--- win005.c
+++ win005.c
@@ -14,12 +14,25 @@ void printDecimalArray(int *A, int n) {
     printf("}\n");
 }
 
+int sumInt(int *A, int n) {
+    int s = 0;
+    for (int i = 0; i < n; i++)
+        s += A[i];
+    return s;
+}
+
+int roulette(int *A, int n) {
+    return 0;
+}
+
 int main(void) {
     int t;
+    int n1 = 10;
     int sample1[] = {8, 4, 1, 2, 0, 3, 9, 6, 7, 5};
     t = (unsigned)time(NULL);
     srand(t);
     printDecimal(t);
-    printDecimalArray(sample1, 10);
+    printDecimalArray(sample1, n1);
+    printDecimal(sumInt(sample1, n1));
     return 0;
 }
