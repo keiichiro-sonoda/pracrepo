436f4e2bfc1129fc68784144ca2d7abf6e4c19ac
keiichiro-sonoda <mountain13577749@gmail.com>
2020-08-22 21:52:10 +0900

    ルーレットっぽいの作った(二度目)

--- win005.c
+++ win005.c
@@ -23,8 +23,12 @@ int sumInt(int *A, int n) {
 
 int roulette(int *A, int n) {
     int r = rand() % sumInt(A, n);
-    printDecimal(r);
-    return 0;
+    int i;
+    // 乱数は非負であるからループに必ず入るはず
+    for (i = 0; i < n && r >= 0; i++)
+        r -= A[i];
+    // iは0でないと思う
+    return i - 1;
 }
 
 int main(void) {
@@ -33,10 +37,9 @@ int main(void) {
     int sample1[] = {8, 4, 1, 2, 0, 3, 9, 6, 7, 5};
     t = (unsigned)time(NULL);
     srand(t);
-    // 最初の乱数が乱数っぽくないので一回作る
-    rand();
     printDecimal(t);
     printDecimalArray(sample1, n1);
     roulette(sample1, n1);
+    printDecimal(roulette(sample1, n1));
     return 0;
 }
