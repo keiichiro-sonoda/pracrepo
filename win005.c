c3a9232670c4f5aa0d6f5f38e8ffe1f02d2c002e
keiichiro-sonoda <mountain13577749@gmail.com>
2020-08-22 22:32:17 +0900

    zeros() とか作った

--- win005.c
+++ win005.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 #define printDecimal(x) printf("%d\n", x)
+#define arrayLength(A) sizeof(A) / sizeof(A[0])
 
 void printDecimalArray(int *A, int n) {
     putchar('{');
@@ -12,6 +13,11 @@ void printDecimalArray(int *A, int n) {
             printf(", ");
     }
     printf("}\n");
+}
+
+void zeros(int *A, int n) {
+    for (int i = 0; i < n; i++)
+        A[i] = 0;
 }
 
 int sumInt(int *A, int n) {
@@ -32,14 +38,15 @@ int roulette(int *A, int n) {
 }
 
 int main(void) {
-    int t;
-    int n1 = 10;
+    int n1, t;
     int sample1[] = {8, 4, 1, 2, 0, 3, 9, 6, 7, 5};
+    n1 = arrayLength(sample1);
+    int result[n1];
+    zeros(result, n1);
     t = (unsigned)time(NULL);
-    srand(t);
     printDecimal(t);
+    srand(t);
     printDecimalArray(sample1, n1);
-    roulette(sample1, n1);
-    printDecimal(roulette(sample1, n1));
+    printDecimalArray(result, n1);
     return 0;
 }
