568b0bf4b2b6f835c1f0e775e312c4bafbb3a52d
keiichiro-sonoda <mountain13577749@gmail.com>
2020-08-22 21:19:42 +0900

    ミスってコミット

--- win005.c
+++ win005.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define printDecimal(x) printf("%d\n", x)
 
 int main(void) {
     int r;
+    srand((unsigned)time(NULL));
     r = rand();
     printDecimal(r);
     return 0;
