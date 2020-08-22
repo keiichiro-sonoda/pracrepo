1282fa0506b9a69120cbdfea048e6181eec5f365
keiichiro-sonoda <mountain13577749@gmail.com>
2020-08-22 21:30:58 +0900

    配列表示関数を作った 無駄な気もする 同じことを何度も繰り返す

--- win005.c
+++ win005.c
@@ -4,13 +4,22 @@
 
 #define printDecimal(x) printf("%d\n", x)
 
+void printDecimalArray(int *A, int n) {
+    putchar('{');
+    for (int i = 0; i < n; i++) {
+        printf("%d", A[i]);
+        if (i < n - 1)
+            printf(", ");
+    }
+    printf("}\n");
+}
+
 int main(void) {
     int t;
+    int sample1[] = {8, 4, 1, 2, 0, 3, 9, 6, 7, 5};
     t = (unsigned)time(NULL);
     srand(t);
     printDecimal(t);
-    for (int i = 0; i < 3; i++) {
-        printDecimal(rand());
-    }
+    printDecimalArray(sample1, 10);
     return 0;
 }
