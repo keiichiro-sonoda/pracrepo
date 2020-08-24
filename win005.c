fb4973b3f831dc2f4caf49977b6837a82805e2ea
keiichiro-sonoda <mountain13577749@gmail.com>
2020-08-24 17:02:56 +0900

    配列の要素を一つ削除する関数を作成

--- win005.c
+++ win005.c
@@ -37,10 +37,25 @@ int roulette(int *A, int n) {
     return i - 1;
 }
 
+// 配列の要素を1つ削除する
+// 削除後の配列は元の配列の長さ-1とする
+void delElementInt(int *src, int *dst, int src_len, int index) {
+    int j = 0;
+    for (int i = 0; i < src_len; i++) {
+        // 該当する添え字以外コピー
+        if (i != index) {
+            dst[j] = src[i];
+            j++;
+        }
+    }
+}
+
 int main(void) {
     int n1, t;
     int sample1[] = {8, 4, 1, 2, 0, 3, 9, 6, 7, 5};
     n1 = arrayLength(sample1);
+    // 削除後配列
+    int sample2[n1 - 1];
     int result[n1];
     zeros(result, n1);
     t = (unsigned)time(NULL);
@@ -50,5 +65,7 @@ int main(void) {
     for (int i = 0; i < 4500000; i++)
         result[roulette(sample1, n1)]++;
     printDecimalArray(result, n1);
+    delElementInt(sample1, sample2, n1, 0);
+    printDecimalArray(sample2, n1 - 1);
     return 0;
 }
