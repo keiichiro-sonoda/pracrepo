9c30b6b86a33a512ab22f34dedbc5eb57dc6f7ac
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-10-16 13:12:51 +0900

    use time as seed

--- test002.c
+++ test002.c
@@ -1,9 +1,10 @@
 // this is a source file for debugging
 #include <stdlib.h>
+#include <time.h>
 #include "sort01.h"
 
 int main(void) {
-    srand((unsigned)123);
+    srand((unsigned)time(NULL));
     sortTest();
     return 0;
 }
