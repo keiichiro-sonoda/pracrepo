59d1f2db77335f0b83bbb30819900e2d14b5a48f
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-10-16 15:56:55 +0900

    oneToOneNormalSprmRoulette was created

--- genetic07.c
+++ genetic07.c
@@ -23,10 +23,60 @@ Board getBoardForBlackSimpleRoulette(Board *next_boards, int n, const Sprm *prp)
     }
     printFloatArray(exp_points, n);
     s = sumFloat(exp_points, n);
-    printFloat(s);
     choosed = rouletteFloat(exp_points, n, s);
-    rouletteFloatTest(exp_points, n);
     return next_boards[choosed];
+}
+
+// return winner
+// the next board is decided by roulette
+int oneToOneNormalSprmRoulette(const Sprm *spp, const Sprm *gpp) {
+    Board nba[NEXT_MAX];
+    int kc[3];
+    int pass = 0;
+    int n, dif;
+    // set turn
+    int turn = 0b01;
+    // set initial board
+    Board main_board = START;
+    while (1) {
+        // calculate next
+        n = nextBoardNormal2(main_board, nba, kc);
+        showBoard(main_board);
+        // can't put a piece anywhere
+        if (n == 0) {
+            // can't do anything one another
+            if (pass) {
+                //printf("end\n");
+                break;
+            }
+            // pass
+            //printf("pass\n");
+            swapNormalizeBoard(&main_board);
+            turn ^= 0b11;
+            pass = 1;
+            continue;
+        }
+        pass = 0;
+        // determine a next board
+        // black (first)
+        if (turn == 0b01) {
+            printf("black\n");
+            main_board = getBoardForBlackSimpleRoulette(nba, n, spp);
+        } // white (second)
+        else {
+            printf("white\n");
+            main_board = getBoardForBlackSimpleRoulette(nba, n, gpp);
+        }
+        // switch the turn
+        turn ^= 0b11;
+    }
+    // difference between black and white
+    dif = kc[1] - kc[2];
+    //printDecimal(dif);
+    if (dif > 0) return turn;
+    if (dif < 0) return turn ^ 0b11;
+    // draw
+    return 0;
 }
 
 int main(void) {
@@ -42,9 +92,6 @@ int main(void) {
     Sprm sample_prm;
     makeSprmSample(&sample_prm);
     printFloatArray(sample_prm.weight, SPRM_LEN);
-    nc = nextBoardNormal2(SAMPLE1, nbs, kc);
-    //showBoardArray(nbs, nc);
-    yeah = getBoardForBlackSimpleRoulette(nbs, nc, &sample_prm);
-    showBoard(yeah);
+    oneToOneNormalSprmRoulette(&sample_prm, &sample_prm);
     return 0;
 }
