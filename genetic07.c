82702e995a81591480cb96ff20165a600a659c10
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-10-16 13:40:58 +0900

    reset seed

--- genetic07.c
+++ genetic07.c
@@ -1,9 +1,12 @@
 // use roulette to decide next action!
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "genetic02.h"
 #include "sort01.h"
 
 int main(void) {
+    srand(123U);
     printf("Hello GA!\nLong time no see!\n");
     sortTest();
     return 0;
