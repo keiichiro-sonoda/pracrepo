04028484540706d5a7acb6112b9267d6f1ec3daf
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-10-21 13:23:37 +0900

    nextGenerationSprmRltRlt was deleted

--- genetic08.c
+++ genetic08.c
@@ -83,82 +83,6 @@ void getSurvivorSprmRRSubMin(const Sprm *generation, Sprm *survivors) {
     checkSprmStatistics(survivors, SURVIVE_NUM);
 }
 
-// make next generation file
-// select survivors by roulette
-int nextGenerationSprmRltRlt(int gene_num, int safety) {
-    int i, j, count;
-    char fnamer[FILENAME_MAX], fnamew[FILENAME_MAX];
-    // previous survivors
-    Sprm parents[SURVIVE_NUM];
-    FILE *fp;
-    // the file name to be read
-    snprintf(fnamer, FILENAME_MAX, FNF_RLTRLT, gene_num);
-    // the file name to be written
-    snprintf(fnamew, FILENAME_MAX, FNF_RLTRLT, gene_num + 1);
-    // view the file names
-    printf("read file : %s\n", fnamer);
-    printf("write file: %s\n", fnamew);
-    // read parents
-    if ((fp = fopen(fnamer, "rb")) == NULL) {
-        printf("\a%s can't be opened\n", fnamer);
-        return -1;
-    }
-    // opened!
-    fread(parents, sizeof parents, 1, fp);
-    fclose(fp);
-    // allow overwriting
-    if (!safety);
-    // check the file to be written (can read?)
-    else if ((fp = fopen(fnamew, "rb")) != NULL) {
-        printf("\a%s exists. Do you overwrite it? (y\\n): ", fnamew);
-        fclose(fp);
-        // don't overwrite
-        if (getchar() != 121) {
-            while (getchar() != 10);
-            printf("terminated\n");
-            return -1;
-        }
-        if (getchar() != 10) {
-            while (getchar() != 10);
-            printf("terminated\n");
-            return -1;
-        }
-    }
-    // store a generation
-    Sprm generation[GENE_NUM];
-
-    // 10 parents copy (elite selection)
-    for (count = 0; count < SURVIVE_NUM; count++)
-        generation[count] = parents[count];
-
-    // crossing! (use elite genes)
-    // 45 combinations, 2 children per couple
-    for (i = 0; i < SURVIVE_NUM - 1; i++) {
-        for (j = i + 1; j < SURVIVE_NUM; j++) {
-            // average
-            generation[count] = makeChildAverageSprm(parents[i], parents[j]);
-            count++;
-            // cross (the mutation rate is 5%)
-            generation[count] = makeChildCrossMSprm(parents[i], parents[j]);
-            count++;
-        }
-    }
-    // calcurate survivors
-    Sprm survivors[SURVIVE_NUM];
-    // battle! (the next board is decided by roulette)
-    getSurvivorSprmRltRlt(generation, survivors);
-
-    // write current survivors to the file
-    if ((fp = fopen(fnamew, "wb")) == NULL) {
-        printf("\a%s cannot be opened\n", fnamew);
-        return -1;
-    }
-    // opened!
-    fwrite(survivors, sizeof survivors, 1, fp);
-    fclose(fp);
-    return 0;
-}
-
 int main(void) {
     // initialize global variables
     setIndexes();
@@ -172,6 +96,5 @@ int main(void) {
     checkSprmFile(FNF_RLTRLT, 100);
     // test
     nextGenerationSprmFlexLoopFlex(getSvr, nGeneF, 1, 100, 2);
-    nextGenerationSprmLoopFlex(nextGenerationSprmRltRlt, 1, 100, 2);
     return 0;
 }
