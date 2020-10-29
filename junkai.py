0a347a6b51376bc7d6e1b3bb07753797fd862e69
keiichiro-sonoda <mountain13577749@gmail.com>
2020-10-29 10:25:20 +0900

    index を使ってみた

--- junkai.py
+++ junkai.py
@@ -1,3 +1,15 @@
 # 巡回セールスマン問題を解きたい
 # 並び替えの交叉は01のときと異なるのでその練習をしたい
-print("巡回セールスマン問題")
+import random as rd
+
+LENGTH = 6
+
+father = [2, 4, 1, 3, 6, 5]
+mother = [3, 2, 5, 4, 1, 6]
+
+r = rd.randint(0, LENGTH - 1)
+
+def circularCrossing():
+    pass
+
+print(father.index(4))
