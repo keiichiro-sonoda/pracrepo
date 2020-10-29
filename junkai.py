f28cfc23410eae0d3c7f9551dba86c1d6e0f8c85
keiichiro-sonoda <mountain13577749@gmail.com>
2020-10-29 10:28:28 +0900

    __main__ かどうかの確認をする ランダムな添え字を入手してその値を得られた

--- junkai.py
+++ junkai.py
@@ -2,14 +2,22 @@
 # 並び替えの交叉は01のときと異なるのでその練習をしたい
 import random as rd
 
+# 遺伝子長
+# つまり拠点の数
 LENGTH = 6
 
 father = [2, 4, 1, 3, 6, 5]
 mother = [3, 2, 5, 4, 1, 6]
 
-r = rd.randint(0, LENGTH - 1)
+# 親を2つ与える
+def circularCrossing(p1, p2):
+    # ランダムな添え字を一つ選ぶ
+    r = rd.randint(0, LENGTH - 1)
+    # 添え字とそれぞれの値を確認
+    print(r, p1[r], p2[r])
 
-def circularCrossing():
-    pass
+def main():
+    circularCrossing(father, mother)
 
-print(father.index(4))
+if __name__ == "__main__":
+    main()
