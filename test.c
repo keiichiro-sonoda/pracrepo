f6e372a930601484f356fb0ffabdaab67d2621b6
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-11-04 15:16:04 +0900

    checking existence was successed??

--- test.c
+++ test.c
@@ -18,6 +18,6 @@ int main(void) {
         samp2.weight[i] = 0.111f;
     //makeFirstGeneFileFlex(FNF_TEST);
     checkSprmFile(FNF_TEST, 0);
-    nGeneSprmSaveAll(FNF_TEST, 0, 1);
+    printDecimal(nGeneSprmSaveAll(FNF_TEST, 0, 1));
     return 0;
 }
