39b8a835a151de5d0d095ff6cff406eed06a847b
keiichiro-sonoda <mountain13577749@gmail.com>
2020-11-09 11:43:26 +0900

    test_win.c を追加し忘れていた

---
+++ test_win.c
@@ -0,0 +1,17 @@
+// デバッグ用ファイル?
+#include <stdio.h>
+#include <stdlib.h>
+#include "genetic02_win.h"
+
+int main(int argc, char **argv) {
+    // seed reset
+    srand(123U);
+    //srand((unsigned)time(NULL));
+    // set global variable
+    setCORR_TABLE();
+    // set initial board
+    initBoard();
+    showBoard(START);
+    checkSprmFile(0);
+    return 0;
+}
