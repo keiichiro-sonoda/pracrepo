39b35fb47e80bc7454b6e333fd81f6bd517a772d
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-11-10 13:48:51 +0900

    rouletteAveUni probably worked as expected it's prbably reproducible

--- genetic09.c
+++ genetic09.c
@@ -6,11 +6,38 @@
 #include "sort01.h"
 #include "genetic02.h"
 
+// roulette selection
+// perform averaging and uniform crossing once for each parents
+void rouletteAveUni(const int *fitness, const int *numbers, const Sprm *current, Sprm *next) {
+    int count;
+    int parents[2];
+    // elite selection
+    for (count = 0; count < ELITE_NUM; count++)
+        next[count] = current[numbers[count]];
+    // selection and crossover
+    while (count < POPULATION) {
+        // choose parents by roulette selection
+        // don't allow duplication
+        rouletteIntMltDep(fitness, POPULATION, parents, 2);
+        // make a child (average)
+        next[count] = makeChildAverageSprm(current[numbers[parents[0]]], current[numbers[parents[1]]]);
+        count++;
+        // overflow
+        if (count >= POPULATION) break;
+        // make a child (uniform crossover)
+        next[count] = makeChildCrossMSprm(current[numbers[parents[0]]], current[numbers[parents[1]]]);
+        count++;
+    }
+}
+
 int main(void) {
     srand(123U);
     //srand((unsigned)time(NULL));
     setIndexes();
     initBoard();
-    printString("debugging");
+    char format[] = FNF_05006001000005;
+    //makeFirstGeneFileFlex(format);
+    checkSprmFile(format, 2);
+    nGeneSSAFlexLoop(rouletteAveUni, format, 1, 0, 2);
     return 0;
 }
