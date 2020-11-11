d043cf68a389c8d6f50aa9b1c23d5067d19a04da
keiichiro-sonoda <mountain13577749@gmail.com>
2020-11-11 13:38:28 +0900

    関数化した 分からない事もあるが, 可視化できそうなことはわかった

--- test002.py
+++ test002.py
@@ -1,18 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 10, 0.05)
-y = np.arange(0, 10, 0.05)
+def view2DArrSamp1():
+    x = np.arange(0, 10, 0.05)
+    y = np.arange(0, 10, 0.05)
 
-X, Y = np.meshgrid(x, y)
-Z = np.sin(X) + np.cos(Y)
+    X, Y = np.meshgrid(x, y)
+    Z = np.sin(X) + np.cos(Y)
 
-plt.pcolormesh(X, Y, Z, cmap='hsv')
+    # 等高線図の生成. cmapで色付けの規定を指定する.
+    #plt.pcolormesh(X, Y, Z, cmap='hsv')
+    plt.pcolor(X, Y, Z, cmap='hsv')
 
-pp = plt.colorbar(orientation="vertical")
-pp.set_label("Label", fontname="Arial", fontsize=24)
+    pp = plt.colorbar(orientation="vertical")
+    pp.set_label("Label", fontname="Arial", fontsize=24)
 
-plt.xlabel('X', fontsize=24)
-plt.ylabel('Y', fontsize=24)
+    plt.xlabel('X', fontsize=24)
+    plt.ylabel('Y', fontsize=24)
 
-plt.show()
+    plt.show()
+
+if __name__ == "__main__":
+    view2DArrSamp1()
