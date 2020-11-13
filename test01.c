9155de69e9b4ea8dcd7ee605fc0f2b01a1ce6ab3
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-11-13 14:04:38 +0900

    randMutArray probably worked as expected

--- test01.c
+++ test01.c
@@ -6,12 +6,22 @@
 #include "sort01.h"
 
 int main(void) {
-    srand(123U);
+    srand(127U);
     //srand((unsigned)time(NULL));
     initBoard();
-    makeFGFilePrm1L(FNF_TEST);
+    //makeFGFilePrm1L(FNF_TEST);
     //showPrm1L(pra[2]);
     //leagueMatchPrm1LFlex(getBoardForBlackPrm1LRlt, pra, fitness);
-    nGenePrm1L(rltUniRd, FNF_TEST, 0, 1);
+    //nGenePrm1L(rltUniRd, FNF_TEST, 0, 1);
+    float test1[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+    int l1 = arrayLength(test1);
+    float test2[l1], test3[l1];
+    zerosFloat(test2, l1);
+    printFloatArray(test1, l1);
+    printFloatArray(test2, l1);
+    uniCrossArray(test1, test2, test3, l1);
+    printFloatArray(test3, l1);
+    randMutArray(test3, l1);
+    printFloatArray(test3, l1);
     return 0;
 }
