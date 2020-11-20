52d3d81a5144e85ebde3e1a663927401f2edaca0
Keiichiro Sonoda <mountain13577749@gmail.com>
2020-11-20 13:35:06 +0900

    test02.c was modified to get random numbers

--- test02.c
+++ test02.c
@@ -7,10 +7,15 @@
 #include "genetic02.h"
 
 int main(void) {
-    srand(123U);
-    //srand((unsigned)time(NULL));
+    srand(SEED);
+    srand((unsigned)time(NULL));
     setIndexes();
     initBoard();
+    int pair[2];
+    int points[6] = {6, 5, 4, 3, 2, 1};
+    //randIntDoubleDep(pair, 1, 6);
+    rouletteIntMltDep(points, 6, pair, 2);
+    printDecimalArray(pair, 2);
     printString("debugging");
     return 0;
 }
