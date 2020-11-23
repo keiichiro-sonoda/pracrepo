857b08b4b6c0dcd2669d31b24749111483471c3b
keiichiro-sonoda <mountain13577749@gmail.com>
2020-11-23 22:03:51 +0900

    比較用の２つのグラフを作って保存した

--- yeah001.py
+++ yeah001.py
@@ -5,8 +5,11 @@ print(mpl.rcParams["figure.figsize"])
 print(mpl.rcParams["figure.dpi"])
 print([mpl.rcParams["figure.subplot." + name] for name in ["left", "right", "top", "bottom", "wspace", "hspace"]])
 
-fig = plt.figure(facecolor="#ffffff")
+fig = plt.figure(facecolor="#ff0000")
 ax = fig.add_subplot(111)
 
 print(fig)
 print(ax)
+
+fig.savefig("//home//sonoda//Pictures//001.png", facecolor="#00ff00")
+fig.savefig("//home//sonoda//Pictures//001_tight.png", facecolor="#00ff00", bbox_inches="tight")
