6f477c5d13303918368dde26f4d2a54647d5f6e4
keiichiro-sonoda <mountain13577749@gmail.com>
2020-11-23 21:59:09 +0900

    matplotlib と matplotlib.pyplot は別物? クラスかインスタンスかの違いなのか?

--- yeah001.py
+++ yeah001.py
@@ -1,5 +1,12 @@
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 print(mpl.rcParams["figure.figsize"])
 print(mpl.rcParams["figure.dpi"])
 print([mpl.rcParams["figure.subplot." + name] for name in ["left", "right", "top", "bottom", "wspace", "hspace"]])
+
+fig = plt.figure(facecolor="#ffffff")
+ax = fig.add_subplot(111)
+
+print(fig)
+print(ax)
