fb5783252a26a463229a6bd4fef59d40272559bc
keiichiro-sonoda <mountain13577749@gmail.com>
2020-11-23 22:09:58 +0900

    なんかすごいグラフ?画像が表示されたぞ

--- yeah001.py
+++ yeah001.py
@@ -5,13 +5,13 @@ print(mpl.rcParams["figure.figsize"])
 print(mpl.rcParams["figure.dpi"])
 print([mpl.rcParams["figure.subplot." + name] for name in ["left", "right", "top", "bottom", "wspace", "hspace"]])
 
-fig = plt.figure(facecolor="#ff0000")
-ax = fig.add_subplot(111)
+fig = plt.figure(facecolor="skyblue", linewidth=100, edgecolor="green")
+ax = fig.add_subplot(111, ylabel="ylabel")
+fig.subplots_adjust(left=0, right=1, bottom=0.2, top=0.8)
 
 print(fig)
 print(ax)
-
-fig.savefig("//home//sonoda//Pictures//001.png", facecolor="#00ff00")
-fig.savefig("//home//sonoda//Pictures//001_tight.png", facecolor="#00ff00", bbox_inches="tight")
+fig.savefig("//home//sonoda//Pictures//002.png", facecolor="#0000ff", edgecolor="orange")
+fig.savefig("//home//sonoda//Pictures//002_tight.png", facecolor="#0000ff", edgecolor="#ff00ff", bbox_inches="tight")
 
 plt.show()
