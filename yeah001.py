062b08ef5b38f92d2eae605a42f613486a6ae307
keiichiro-sonoda <mountain13577749@gmail.com>
2020-11-23 21:54:48 +0900

    matplotlibのデフォルトパラメータを確認してみた

--- yeah001.py
+++ yeah001.py
@@ -1,3 +1,5 @@
 import matplotlib as mpl
 
 print(mpl.rcParams["figure.figsize"])
+print(mpl.rcParams["figure.dpi"])
+print([mpl.rcParams["figure.subplot." + name] for name in ["left", "right", "top", "bottom", "wspace", "hspace"]])
