d02f5d763f608cd854a394d6f71e00fa181dc45e
keiichiro-sonoda <mountain13577749@gmail.com>
2020-11-23 22:11:51 +0900

    subplot_adjust の意味が少しわかった気がする 基本的に0~1に収まるようにすればきれいにできそう

--- yeah001.py
+++ yeah001.py
@@ -7,11 +7,9 @@ print([mpl.rcParams["figure.subplot." + name] for name in ["left", "right", "top
 
 fig = plt.figure(facecolor="skyblue", linewidth=100, edgecolor="green")
 ax = fig.add_subplot(111, ylabel="ylabel")
-fig.subplots_adjust(left=0, right=1, bottom=0.2, top=0.8)
+fig.subplots_adjust(left=0.2, right=0.8, bottom=0.2, top=0.8)
 
 print(fig)
 print(ax)
-fig.savefig("//home//sonoda//Pictures//002.png", facecolor="#0000ff", edgecolor="orange")
-fig.savefig("//home//sonoda//Pictures//002_tight.png", facecolor="#0000ff", edgecolor="#ff00ff", bbox_inches="tight")
 
 plt.show()
