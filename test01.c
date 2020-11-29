17ac43505b6189df765598749487f67e74a9a277
keiichiro-sonoda <mountain13577749@gmail.com>
2020-11-29 21:06:45 +0900

    一様交叉のサンプルを試してみた これを図にしたい

--- test01.c
+++ test01.c
@@ -17,6 +17,14 @@ int main(void) {
     //nGenePrm1LLoopSeed(rltUniRd, format, 0, 0, 500);
     // for debugging
     //makeFGFilePrm1LComp(format);
-    nGenePrm1LCompLoop(rltUniRd, format, 1, 200, 401);
+    //nGenePrm1LCompLoop(rltUniRd, format, 1, 200, 401);
+    int sgl = 10;
+    float sg1[sgl], sg2[sgl], sg3[sgl];
+    randWeightArray(sg1, sgl);
+    randWeightArray(sg2, sgl);
+    uniCrossArray(sg1, sg2, sg3, sgl);
+    printFloatArray(sg1, sgl);
+    printFloatArray(sg2, sgl);
+    printFloatArray(sg3, sgl);
     return 0;
 }
