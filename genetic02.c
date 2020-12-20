5ef230d7c676d63613b89be65b49640e531f0e0e
keiichiro-sonoda <mountain13577749@gmail.com>
2020-12-20 21:17:52 +0900

    多分想定通りの交叉できてるんじゃないかな?

--- genetic02.c
+++ genetic02.c
@@ -400,9 +400,10 @@ Sprm blendCrossSprmComp(const Sprm *mother_p, const Sprm *father_p) {
 
 // 交叉テスト用関数
 void crossTestSprm(void) {
+    srand((unsigned)time(NULL));
     Sprm pr1, pr2, pra[2];
-    initArrayConst(pr1.weight, SPRM_LEN, .222f);
-    initArrayConst(pr2.weight, SPRM_LEN, .111f);
+    initArrayConst(pr1.weight, SPRM_LEN, -.5000f);
+    initArrayConst(pr2.weight, SPRM_LEN, .500f);
     printf("親1: ");
     showSprmOneLine(pr1);
     printf("親2: ");
