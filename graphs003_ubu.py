90192fba66e83fec03d72e4218c1de2a8b433474
keiichiro-sonoda <mountain13577749@gmail.com>
2020-12-20 12:40:31 +0900

    シード1, 公比exp(0.005)はシードで値が変わってくるね

--- graphs003_ubu.py
+++ graphs003_ubu.py
@@ -772,7 +772,7 @@ def main():
     elite_num = 1
     crln = 0.005
     gene_num = 50
-    seed = 365
+    seed = 123
     #viewFitnessGraph2(population, elite_num, -0.02, 0.02, 0.001, gene_num, seed, grid=True)
     viewFitnessGraph3(population, 50, elite_num, crln, seed, 0, 100, grid=True)
     plt.show()
