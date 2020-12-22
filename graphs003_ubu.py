601c7ac81985a80e56b6a423dd928af137fd67f1
keiichiro-sonoda <mountain13577749@gmail.com>
2020-12-22 12:16:26 +0900

    BLX-α 交叉のグラフを描画できた 10, 20, 50, 100 世代を保存した シード124, エリート1, -0.020, -0.019, ..., 0.020

--- graphs003_ubu.py
+++ graphs003_ubu.py
@@ -666,7 +666,7 @@ def main():
     seed = 124
     viewFitnessGraph2(population, elite_num, crs_id, -0.02, 0.02, 0.001, gene_num, seed, grid=True)
     #viewFitnessGraph3(population, 50, elite_num, crln, seed, 0, 100, grid=True)
-    plt.show()
+    #plt.show()
     print("終わり")
 
 if __name__ == "__main__":
