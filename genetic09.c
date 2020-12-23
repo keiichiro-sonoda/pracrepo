9504b669adf4492559f3d02391523658f53203a9
keiichiro-sonoda <mountain13577749@gmail.com>
2020-12-23 17:27:29 +0900

    初期世代雑魚が1以外のときは, ちゃんと乱数で作成されているっぽい

--- genetic09.c
+++ genetic09.c
@@ -367,6 +367,7 @@ int main(void) {
     //sortTest();
     //trySomeCommonRatio(-2.0, 2.09, 0.05, 101);
     //crossTestSprm();
+    _makeFGFileSprmComp(FNAME_TEST);
     Sprm pra[POPULATION];
     loadSprmFileCompDirect(FNAME_TEST, pra, POPULATION);
     showFamilyPart(pra);
