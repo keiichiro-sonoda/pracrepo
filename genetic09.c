e234421eae9b3fae3906d446ca9cb54d29ad2160
keiichiro-sonoda <mountain13577749@gmail.com>
2020-12-24 21:27:05 +0900

    4種類の解像度を試したい 3万ファイルくらい増えそう だけどやる 明日PC壊れてたら悲しいね

--- genetic09.c
+++ genetic09.c
@@ -387,6 +387,9 @@ int main(void) {
     */
     // 修正用
     //sortOnlySprmComp(scm, format, 0);
-    //trySomeCommonRatio(-2000, 0, 50, 101, 1);
+    trySomeCommonRatio(-20, 20, 1, 101, 1);
+    trySomeCommonRatio(-100, 100, 5, 101, 1);
+    trySomeCommonRatio(-2000, 2000, 100, 101, 1);
+    trySomeCommonRatio(-8000, 8000, 400, 101, 1);
     return 0; 
 }
