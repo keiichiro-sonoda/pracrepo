192870dc8b06f85ea77033ac3aa0b538d93d2fed
keiichiro-sonoda <mountain13577749@gmail.com>
2020-12-24 12:19:56 +0900

    40分程度で完了した 非正規化ばんざい

--- genetic09.c
+++ genetic09.c
@@ -393,6 +393,6 @@ int main(void) {
     */
     // 修正用
     //sortOnlySprmComp(scm, format, 0);
-    //trySomeCommonRatio(0, -2000, -100, 31, 1);
+    trySomeCommonRatio(0, -2000, -50, 51, 1);
     return 0; 
 }
