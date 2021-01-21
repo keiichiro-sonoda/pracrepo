dae64e0f1d3352d29d92048f11dea593b6d89abd
keiichiro-sonoda <mountain13577749@gmail.com>
2021-01-21 23:02:51 +0900

    やはりなかなか目盛りのサイズが変わらない

--- graphs003_ubu.py
+++ graphs003_ubu.py
@@ -603,13 +603,14 @@ def viewFitnessGraph4(loc_pop, loc_eln, crs_id, lncr_start, lncr_stop, lncr_step
     X, Y = np.meshgrid(x, y)
     Z = np.array(medfl).reshape(len(x), -1).T
     #fig = plt.figure(figsize=(8, 5))
-    magnitude = 0.7
+    magnitude = 0.3
     fig = plt.figure(figsize=(8 * magnitude, 5 * magnitude))
     #fig = plt.figure(figsize=(5, 3.125))
     ax = fig.add_subplot(111)
     ax.set_xlabel("the natural log of common ratio", fontsize=15*magnitude)
     ax.set_ylabel("generation", fontsize=15*magnitude)
-    ax.set_xticklabels(x, fontsize=15*magnitude)
+    #ax.set_xticklabels(fontsize=14*magnitude)
+    ax.tick_params(axis="x", labelsize=15*magnitude)
     mappable = ax.pcolor(X, Y, Z, vmin=v_min, vmax=v_max, cmap=cm, shading="nearest")
     fig.colorbar(mappable, ax=ax)
     fig.tight_layout()
