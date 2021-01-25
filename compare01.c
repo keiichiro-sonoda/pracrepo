a7400f62b010adb85f2ce5d777be730e8d3e8f78
keiichiro-sonoda <mountain13577749@gmail.com>
2021-01-25 14:25:14 +0900

    あらかじめファイル数を調べるためにループを追加した

--- compare01.c
+++ compare01.c
@@ -10,10 +10,13 @@
 // 公比の自然対数は1000倍した整数値で与える
 // まずは白黒1回ずつ, 手は最大評価値確定でいこう
 void vsOtherCommonRatio(int start_th, int stop_th, int step_th, int gene_num) {
-    int i;
+    int i, count;
     double loc_cr_ln;
     char format[FILENAME_MAX];
-    for (i =  start_th; i <= stop_th; i += step_th) {
+    count = 0;
+    for (i = start_th; i <= stop_th; i += step_th) count++;
+    printDecimal(count);
+    for (i = start_th; i <= stop_th; i += step_th) {
         loc_cr_ln = (double)i / 1000;
         makeSprmFileFormatAuto(format, FILENAME_MAX, 0b010, 1, 50, 0, 3, 5, 0, .01, COMPARE_SEED, loc_cr_ln, 0);
         puts(format);
