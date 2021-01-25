c3bfae4c9e398a1063d18f9fc27b4a69f67e78ec
keiichiro-sonoda <mountain13577749@gmail.com>
2021-01-25 14:23:08 +0900

    多分公比ごとにファイルフォーマット作れた

--- compare01.c
+++ compare01.c
@@ -3,15 +3,20 @@
 */
 #include "genetic02.h"
 
+// 比較対象のシード値
+#define COMPARE_SEED 555
+
 // 公比どうしリーグ戦?
 // 公比の自然対数は1000倍した整数値で与える
 // まずは白黒1回ずつ, 手は最大評価値確定でいこう
 void vsOtherCommonRatio(int start_th, int stop_th, int step_th, int gene_num) {
     int i;
     double loc_cr_ln;
+    char format[FILENAME_MAX];
     for (i =  start_th; i <= stop_th; i += step_th) {
         loc_cr_ln = (double)i / 1000;
-        printFloat(loc_cr_ln);
+        makeSprmFileFormatAuto(format, FILENAME_MAX, 0b010, 1, 50, 0, 3, 5, 0, .01, COMPARE_SEED, loc_cr_ln, 0);
+        puts(format);
     }
 }
 
