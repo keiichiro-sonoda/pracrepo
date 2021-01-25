f21377102da6ea7393da1a5bf49c0ecb6d1fa70e
keiichiro-sonoda <mountain13577749@gmail.com>
2021-01-25 14:29:28 +0900

    重みの1盤の要素は相変わらず綺麗に適応度と比例している?

--- compare01.c
+++ compare01.c
@@ -10,16 +10,22 @@
 // 公比の自然対数は1000倍した整数値で与える
 // まずは白黒1回ずつ, 手は最大評価値確定でいこう
 void vsOtherCommonRatio(int start_th, int stop_th, int step_th, int gene_num) {
-    int i, count;
+    int i, loc_pop, count;
     double loc_cr_ln;
     char format[FILENAME_MAX];
+    loc_pop = 0;
+    for (i = start_th; i <= stop_th; i += step_th) loc_pop++;
+    printDecimal(loc_pop);
+    // 代表者配列
+    Sprm rep_pra[loc_pop];
     count = 0;
-    for (i = start_th; i <= stop_th; i += step_th) count++;
-    printDecimal(count);
-    for (i = start_th; i <= stop_th; i += step_th) {
+    for (i = start_th, count = 0; i <= stop_th; i += step_th, count++) {
         loc_cr_ln = (double)i / 1000;
         makeSprmFileFormatAuto(format, FILENAME_MAX, 0b010, 1, 50, 0, 3, 5, 0, .01, COMPARE_SEED, loc_cr_ln, 0);
         puts(format);
+        rep_pra[count] = loadRepSprmComp(format, gene_num, loc_pop);
+        showSprmOneLine(rep_pra[count]);
+        putchar(10);
     }
 }
 
