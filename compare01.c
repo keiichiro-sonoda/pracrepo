f0dc1d336f4cda338be8300788659b62ba4434d3
keiichiro-sonoda <mountain13577749@gmail.com>
2021-01-25 14:11:35 +0900

    可変引数の場合, ちゃんと指定通りの型で代入しないとなのかな

--- compare01.c
+++ compare01.c
@@ -6,7 +6,7 @@
 int main(void) {
     Sprm test;
     char format[FILENAME_MAX];
-    makeSprmFileFormatAuto(format, FILENAME_MAX, 0b010, 1, 50, 0, 3, 5, 0, .01f, 555, 0.001, 0);
+    makeSprmFileFormatAuto(format, FILENAME_MAX, 0b010, 1, 50, 0, 3, 5, 0, .01, 555, 0.001, 0);
     puts(format);
     test = loadRepSprmComp(format, 100, 50);
     showSprmOneLine(test);
