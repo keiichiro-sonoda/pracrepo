115e651dbc943f80b9a04a421acc2aa78a1fb6ae
keiichiro-sonoda <mountain13577749@gmail.com>
2021-01-25 14:12:46 +0900

    101世代は未ソートになっている

--- compare01.c
+++ compare01.c
@@ -8,7 +8,7 @@ int main(void) {
     char format[FILENAME_MAX];
     makeSprmFileFormatAuto(format, FILENAME_MAX, 0b010, 1, 50, 0, 3, 5, 0, .01, 555, -0.01, 0);
     puts(format);
-    test = loadRepSprmComp(format, 100, 50);
+    test = loadRepSprmComp(format, 101, 50);
     showSprmOneLine(test);
     return 0;
 }
