16465a30633a4219a0c1e452ee5987e48f5dfdd3
keiichiro-sonoda <mountain13577749@gmail.com>
2021-02-02 20:28:20 +0900

    比較対象用パラメータの準備をした

--- genetic02.h
+++ genetic02.h
@@ -12,7 +12,7 @@
 #include "sort01.h"
 
 #ifndef SEED
-#define SEED 555U // シード値
+#define SEED 777U // シード値
 #endif
 
 // ランキング選択で, 等比数列で確率を決定する場合の公比
@@ -67,7 +67,7 @@
 
 #define COMPRESS 1 // 圧縮するか否か
 
-#define ZAKO_FIRST 1 // 全て同一の弱い個体の集団から始めるフラグ
+#define ZAKO_FIRST 0 // 全て同一の弱い個体の集団から始めるフラグ
 
 #define SPRM_LEN 10 // 区別できるマスの数 (個体の配列長)
 
