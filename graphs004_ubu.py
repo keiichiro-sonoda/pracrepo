5ea02848944b4168f11d62a761dd1e0b558d30e2
keiichiro-sonoda <mountain13577749@gmail.com>
2021-02-02 20:27:13 +0900

    標準偏差のグラフもなかなか面白い

--- graphs004_ubu.py
+++ graphs004_ubu.py
@@ -821,7 +821,7 @@ def main():
     #viewWeightMeansMap(50, 0, 5, -2.0, 0.0, 0.05, 0, 100, 555, w_num=6, options=0b01)
     #viewWeightSDMeansMap(50, 0, 5, -2.0, 2.0, 0.1, 0, 100, 555, options=0b00)
     # 初期雑魚で範囲-0.040から0.000まで0.001ずつ
-    viewFitnessGraph4(50, 0, 5, -0.040, 0.000, 0.001, 0, 100, 555, options=0b01, stat_option="median", extention="jpg", prm_dir=PRM_DIR01)
+    viewFitnessGraph4(50, 0, 5, -0.040, 0.000, 0.001, 0, 100, 555, options=0b01, stat_option="stdev", extention="jpg", prm_dir=PRM_DIR01)
     # ファイル名作成テスト
     #fname = slw.makeSprmFileNameRankGeoProgWrap(50, 1, 3, 5, 123, 2.0, 100, options=0b10)
     #path = makeGraphsFileName(fname, "dummy", 0, 100, extention="pdf", c_map=True)
