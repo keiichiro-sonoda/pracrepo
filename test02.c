ca78830ee1a85cb5f2066621811493d5436c097e
keiichiro-sonoda <mountain13577749@gmail.com>
2021-02-03 11:44:45 +0900

    test02をコンパイル

--- test02.c
+++ test02.c
@@ -1,4 +1,5 @@
 // this is a source file for debugging
+// デバッグ用ファイル
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,13 +8,7 @@
 #include "genetic02.h"
 
 int main(void) {
-    // シード設定 (再現可能性のため)
-    srand(SEED);
-    //srand((unsigned)time(NULL));
-    // 初期化関数
-    setIndexes();
-    initBoard();
-    checkSprmFile(FNF_05006000000005, 103);
+    initSprm();
     printString("debugging");
     return 0;
 }
