9e5fa2c8ef8617f5f1e2e952437179f1ecfd3e74
keiichiro-sonoda <mountain13577749@gmail.com>
2021-02-05 10:12:25 +0900

    選択のデモンストレーション関数を作成

--- test02.c
+++ test02.c
@@ -7,19 +7,21 @@
 #include "sort01.h"
 #include "genetic02.h"
 
+void selDemo(double cr, int loc_pop, int n) {
+    puts("選択のデモ");
+    int parents[2];
+    double geo_prog[loc_pop];
+    geoProg(geo_prog, loc_pop, 1., cr);
+    for (int i = 0; i < n; i++) {
+        rouletteDoubleMltDep(geo_prog, loc_pop, parents, 2);
+        printDecimalArray(parents, 2);
+    }
+}
+
 int main(void) {
     initSprm();
     srand(time(NULL));
-    int i, j;
-    int loc_pop = 50, p_num = 2, loop = 24;
-    int parents[p_num], rslt[loc_pop];
-    double cr = exp(-1), geo_prog[loc_pop];
-    geoProg(geo_prog, loc_pop, 1., cr);
-    printFloatArrayExp(geo_prog, loc_pop);
-    for (i = 0; i < loop; i++) {
-        rouletteDoubleMltDep(geo_prog, loc_pop, parents, p_num);
-        printDecimalArray(parents, p_num);
-    }
+    selDemo(exp(-0.1), 50, 3);
     puts("デバッグ中");
     return 0;
 }
